27196286c6e73195c2c14fe823588ef06da23e65
Carlos Rincon <carlosjrinconavila@gmail.com>
2018-04-07 00:15:53 -0500

    Adjusted procfile for heroku

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 const app = express();
 
-app.use("/", serveStatic (path.join (__dirname, '/dist')));
+app.use("/", serveStatic (path.join (__dirname, '/')));
 
 app.get('*', function (req, res) {
-    res.sendFile(__dirname + '/dist/index.html')
+    res.sendFile(__dirname + '/index.html')
 });
 // Create default port to serve the app on
 const port = 5000;
